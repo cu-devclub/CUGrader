6134d2402a974a3e6ee162f77bd1ae1e0d33d7e3
PingZaMostIndiesBOI <73052182+OverZone-Production@users.noreply.github.com>
2025-06-05 23:42:35 +0700

    Student-page layout

--- Frontend/grader-frontend/src/app/student/page.tsx
+++ Frontend/grader-frontend/src/app/student/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Card } from "@/components/ui/card";
 
 export default function page() {
-  return <div>This is the page</div>;
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-16 border border-solid border-gray-300 flex flex-col items-center"></div>
+
+      <div className="flex-1 flex-col">
+        <div className="flex h-16 border border-solid"></div>
+
+        <div className="flex flex-col px-24">
+          <div className="flex h-16 border border-solid p-4">
+            <h1 className="text-2xl font-bold">TA</h1>
+          </div>
+          <div></div>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex h-16 border border-solid"></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
