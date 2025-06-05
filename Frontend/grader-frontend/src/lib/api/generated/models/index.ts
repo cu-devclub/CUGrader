907585389d10c5df7b78816cd07f266e4a42c069
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-06-05 20:44:48 +0700

    feat: add openapi client (autogenerated)

---
+++ Frontend/grader-frontend/src/lib/api/generated/models/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Assistant';
+export * from './ClassList';
+export * from './ClassObject';
+export * from './CreateClassBody';
+export * from './CreateStudent';
+export * from './DeleteStudent';
+export * from './EditClassBody';
+export * from './EditStudent';
+export * from './GroupArray';
+export * from './InstructorInfo';
+export * from './ModelError';
+export * from './SectionArray';
+export * from './SemesterClasses';
+export * from './StudentInfo';
+export * from './Students';
+export * from './TAInfo';
+export * from './TAeditBody';
+export * from './V1CallbackPost200Response';
+export * from './V1CallbackPostRequest';
+export * from './V1ClassesClassesYearSemesterGet200Response';
+export * from './V1ClassesSemestersGet200Response';
+export * from './V1LoginGet200Response';
