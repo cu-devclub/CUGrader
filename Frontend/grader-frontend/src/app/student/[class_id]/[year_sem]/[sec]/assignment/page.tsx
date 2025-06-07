270f0aadadd539e73135fbe7447a1385b20429d2
PingZaMostIndiesBOI <73052182+OverZone-Production@users.noreply.github.com>
2025-06-08 00:12:29 +0700

    Rework on assignment Tab & Class Card

--- Frontend/grader-frontend/src/app/student/[class_id]/[year_sem]/[sec]/assignment/page.tsx
+++ Frontend/grader-frontend/src/app/student/[class_id]/[year_sem]/[sec]/assignment/page.tsx
@@ -1,41 +1,47 @@
 import React from "react";
-import AssignmentCard from "./AssignmentCard";
+import AssignmentCard from "./AssignmentTable";
+import mockDataType from "./mockDataType";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
+  const mockData = mockDataType(); // Mock Up Data TODO: Give real data
+
   return (
     <div className="flex min-h-screen">
-      <div className="w-1/6 border border-solid border-gray-300 flex flex-col items-center bg-gray-300">
-        <div className="min-w-full flex border border-solid px-4 py-4 items-center">
+      <div className="w-1/6 border-gray-300 flex flex-col items-center bg-gray-300">
+        <div className="min-w-full flex px-4 py-4 items-center">
           <button className="mb-4">☰</button>
         </div>
 
-        <div className="min-w-full flex border border-solid px-6 pb-7 gap-y-3 items-start flex-col">
+        <div className="min-w-full flex px-6 pb-7 gap-y-3 items-start flex-col">
           <button>Notification</button>
           <button>Profile</button>
         </div>
 
-        <div className="min-w-full flex border border-solid px-6 pb-7 gap-y-3 items-start flex-col bg-white"></div>
+        <div className="min-w-full flex px-6 pb-7 gap-y-3 items-start flex-col bg-white"></div>
       </div>
 
-      <div className="w-5/6 border border-solid border-gray-300 flex flex-col items-center">
-        <div className="flex flex-row min-w-full h-27 space-x-5 px-24 border border-solid">
-          <button className="flex self-end h-13 w-30 bg-gray-300 justify-start items-center px-2">
+      <div className="w-5/6 border-gray-300 flex flex-col items-center">
+        <div className="flex flex-row min-w-full h-20 space-x-5 px-24">
+          <Button className="flex self-end h-13 w-30 bg-gray-300 justify-start items-center px-2">
             Assignment
-          </button>
+          </Button>
           <button className="self-end h-13 w-30">profile</button>
         </div>
 
-        <div className="w-full h-full mt-7 border border-solid">
-          <div className="w-full h-1/2 border border-solid flex flex-col pl-20 pr-8">
+        <div className="w-full h-full mt-4">
+          <div className="w-full h-1/2 flex flex-col pl-20 pr-8">
             <h1 className="px-4 mb-1">Assigned</h1>
-            <div className="h-full w-full mb-6 bg-gray-300 flex flex-col px-2 py-3 gap-y-2">
-              <AssignmentCard></AssignmentCard>
+            <div className="h-full w-full  flex flex-col">
+              <AssignmentCard data={mockData}></AssignmentCard>
             </div>
           </div>
 
-          <div className="w-full h-1/2 border border-solid flex flex-col pl-20 pr-8">
-            <h1 className="px-4 mb-4">Done</h1>
-            <div className="h-full w-full mb-2 bg-gray-300"></div>
+          <div className="w-full h-1/2 flex flex-col pl-20 pr-8 ">
+            <h1 className="px-4 mb-1">Done</h1>
+            <div className="h-full w-full">
+              <AssignmentCard data={mockData}></AssignmentCard>
+            </div>
           </div>
         </div>
       </div>
--- Frontend/grader-frontend/src/app/student/studentCard.tsx
+++ Frontend/grader-frontend/src/app/student/studentCard.tsx
@@ -2,8 +2,17 @@
 
 import * as React from "react";
 import { Progress } from "@/components/ui/progress";
+import { Card } from "@/components/ui/card";
 
-function studentCard() {
+interface Props {
+  id: number;
+  class_id: number;
+  class_name: string;
+  image: string;
+  semester: string;
+}
+
+function studentCard({ class_id, class_name, image, semester }: Props) {
   const [progress, setProgress] = React.useState(13);
 
   React.useEffect(() => {
@@ -13,21 +22,24 @@ function studentCard() {
 
   return (
     <>
-      <div className="w-60 h-40 shadow rounded bg-white flex-col">
-        <div className="flex border border-solid min-h-4/10 min-w-full">
-          IMAGE HERE
-        </div>
+      <Card className="w-60 h-40 bg-gray-300 p-0 m-0 overflow-hidden relative">
+        <div className="h-full w-full">
+          <div className="flex h-[45%] w-full m-0 p-0 items-center justify-center border border-solid bg-white">
+            IMAGE HERE
+          </div>
+          <div className="flex h-[55%] w-full m-0 p-0 flex-col bg-gray-300">
+            <div className="flex justify-between items-end min-w-full space-x-5 px-4 pt-2">
+              <h1 className="text-md">{class_name}</h1>
+              <h1 className="text-emerald-400 text-lg">{semester}</h1>
+            </div>
 
-        <div className="flex border border-solid min-h-6/10 min-w-full flex-col bg-gray-300">
-          <div className="flex justify-between border border-solid min-w-full px-3 py-2 items-center">
-            <h1 className="text-md">Class name (Sec 1)</h1>
-          </div>
-          <div className="px-3 flex flex-col gap-y-3">
-            <p className="text-xs text-gray-400">230xxx</p>
-            <Progress value={progress} className="h-3 w-[90%]" />
+            <div className="px-3 flex flex-col gap-y-3">
+              <p className="text-xs text-gray-400">{class_id}</p>
+              <Progress value={progress} className="h-3 w-[90%]" />
+            </div>
           </div>
         </div>
-      </div>
+      </Card>
     </>
   );
 }
