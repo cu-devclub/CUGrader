0cfeec7a770628484c5070fef06f568bc2bc244f
katoretb <katoretv@gmail.com>
2025-06-09 05:42:04 +0700

    try reverse lib

--- Auth/versions/v1/models/auth/callback.go
+++ Auth/versions/v1/models/auth/callback.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"fmt"
 
-	"google.golang.org/api/oauth2/v1"
-	"google.golang.org/api/option"
+	oauth2Service "google.golang.org/api/oauth2/v2"
 )
 
-func (am *AuthModel) Callback(code string) (*oauth2.Userinfoplus, error) {
+func (am *AuthModel) Callback(code string) (*oauth2Service.Userinfo, error) {
+
 	token, err := am.GoogleOauth.Exchange(context.Background(), code)
 	if err != nil {
 		return nil, fmt.Errorf("code exchange failed: %w", err)
 	}
 
 	client := am.GoogleOauth.Client(context.Background(), token)
-	oauth2Service, err := oauth2.NewService(context.Background(), option.WithHTTPClient(client))
+	oauth2Service, err := oauth2Service.New(client)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create OAuth2 service: %w", err)
+		return nil, fmt.Errorf("Failed to create OAuth2 service: %w", err)
 	}
 
 	userInfo, err := oauth2Service.Userinfo.Get().Do()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get user info: %w", err)
+		return nil, fmt.Errorf("Failed to get user info: %w", err)
 	}
 
 	return userInfo, nil
