057fcc26bc71f90980df1f3e3b4433bfe7bfc091
PingZaMostIndiesBOI <73052182+OverZone-Production@users.noreply.github.com>
2025-06-10 20:04:26 +0700

    Rework on student page filter

--- Frontend/grader-frontend/src/app/student/page.tsx
+++ Frontend/grader-frontend/src/app/student/page.tsx
@@ -6,10 +6,12 @@ import OldClassCard from "./ClassCard";
 import MockClass from "./mockClass";
 import { ClassCard } from "@/components/class-card";
 import { Plus } from "lucide-react";
+import { SemesterSelector } from "../instructor/semester-selector";
+import { LogOut, Bell } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { api } from "@/lib/api";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 type Assistant = {
@@ -31,60 +33,55 @@ type ClassType = {
 export default function Page() {
   const router = useRouter();
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["ClassData"],
-    queryFn: async () => api.class.listBySemester("2025/1"),
+  const { data: semesterList } = useSuspenseQuery({
+    queryKey: ["semester"],
+    queryFn: () => api.semester.list(),
   });
 
-  // const { data: section } = useQuery({
-  //   queryKey: ["Sec"],
-  //   queryFn: async () => {
-  //     const res = await api.section;
-  //     return res;
-  //   },
-  // });
+  const [selectedSemester, setSelectedSemester] = useState(semesterList[0]);
 
-  console.log(JSON.stringify(data, null, 2));
+  const { data, isLoading } = useSuspenseQuery({
+    queryKey: ["ClassData", semesterList],
+    queryFn: async () => api.class.listBySemester(selectedSemester),
+  });
 
-  // console.log(JSON.stringify(section, null, 2));
+  // const classData = MockClass(); // Use mocked data
 
-  const classData = MockClass(); // Use mocked data
+  // const [displayClassesTA, setDisplayClassesTA] = useState<ClassType[]>([]);
+  // const [displayClasses, setDisplayClasses] = useState<ClassType[]>([]);
 
-  const [displayClassesTA, setDisplayClassesTA] = useState<ClassType[]>([]);
-  const [displayClasses, setDisplayClasses] = useState<ClassType[]>([]);
+  // const semesters = Array.from(
+  //   new Set(classData.assistant.map((cls) => cls.semester))
+  // );
 
-  const semesters = Array.from(
-    new Set(classData.assistant.map((cls) => cls.semester))
-  );
+  // const semestersStudy = Array.from(
+  //   new Set(classData.study.map((cls) => cls.semester))
+  // );
 
-  const semestersStudy = Array.from(
-    new Set(classData.study.map((cls) => cls.semester))
-  );
+  // const changeTAClassDisplay = (sem: string) => {
+  //   if (sem === "all") {
+  //     setDisplayClassesTA(classData.assistant);
+  //   } else {
+  //     const filtered = classData.assistant.filter(
+  //       (cls) => cls.semester === sem
+  //     );
+  //     setDisplayClassesTA(filtered);
+  //   }
+  // };
 
-  const changeTAClassDisplay = (sem: string) => {
-    if (sem === "all") {
-      setDisplayClassesTA(classData.assistant);
-    } else {
-      const filtered = classData.assistant.filter(
-        (cls) => cls.semester === sem
-      );
-      setDisplayClassesTA(filtered);
-    }
-  };
+  // const changeClassDisplay = (sem: string) => {
+  //   if (sem === "all") {
+  //     setDisplayClasses(classData.study);
+  //   } else {
+  //     const filtered = classData.study.filter((cls) => cls.semester === sem);
+  //     setDisplayClasses(filtered);
+  //   }
+  // };
 
-  const changeClassDisplay = (sem: string) => {
-    if (sem === "all") {
-      setDisplayClasses(classData.study);
-    } else {
-      const filtered = classData.study.filter((cls) => cls.semester === sem);
-      setDisplayClasses(filtered);
-    }
-  };
-
-  useEffect(() => {
-    setDisplayClassesTA(classData.assistant);
-    setDisplayClasses(classData.study);
-  }, []);
+  // useEffect(() => {
+  //   setDisplayClassesTA(classData.assistant);
+  //   setDisplayClasses(classData.study);
+  // }, []);
 
   return (
     <div className="flex-col flex min-h-screen items-center">
@@ -93,40 +90,38 @@ export default function Page() {
         <h1 className="text-3xl font-bold text-primary">All Classes</h1>
 
         {/* Right side: Buttons */}
-        <div className="flex items-center space-x-4">
-          <button className="text-xl mr-8">
-            <Plus></Plus>
-          </button>
-          <button className="text-xl">🔔</button>
-          <button className="text-xl">👤</button>
+        <div className="flex items-center space-x-2 pb-2">
+          <Button variant="ghost" className="text-xl mr-8">
+            <Plus />
+          </Button>
+          <Button variant="ghost" className="text-xl">
+            <Bell />
+          </Button>
+          <Button variant="ghost" className="text-xl">
+            <LogOut />
+          </Button>
         </div>
       </div>
 
       <div className="w-[90%] flex flex-col pb-5 border-b">
         <div className="flex h-16 space-x-4 items-center px-4 py-5">
           <h1 className="font-bold">Teacher Assistant</h1>
-          <select
-            className="border rounded px-2"
-            onChange={(e) => changeTAClassDisplay(e.target.value)}
-          >
-            <option value="all">All Semesters</option>
-            {semesters.map((sem) => (
-              <option key={sem} value={sem}>
-                {sem}
-              </option>
-            ))}
-          </select>
+          <SemesterSelector
+            semester={selectedSemester}
+            onSemesterChange={setSelectedSemester}
+            semesterList={semesterList}
+          />
         </div>
 
         <div className="grid gap-7 grid-cols-[repeat(auto-fill,minmax(16rem,20rem))]">
-          {displayClassesTA.map((classTA, index) => (
+          {data.assistant.map((classTA, index) => (
             <ClassCard
               key={index}
               className={classTA.courseName}
               courseId={classTA.courseId.toString()}
               href=""
               name={classTA.courseName}
-              semester={classTA.semester}
+              semester={selectedSemester}
               headerImageUrl={classTA.image}
               menuItems={[]}
             />
@@ -149,27 +144,21 @@ export default function Page() {
       <div className="flex w-[90%] flex-col">
         <div className="flex h-16 items-center px-4 pt-3 space-x-4">
           <h1 className="font-bold">Student</h1>
-          <select
-            className="border rounded px-2 py-1"
-            onChange={(e) => changeClassDisplay(e.target.value)}
-          >
-            <option value="all">All Semesters</option>
-            {semestersStudy.map((sem) => (
-              <option key={sem} value={sem}>
-                {sem}
-              </option>
-            ))}
-          </select>
+          <SemesterSelector
+            semester={selectedSemester}
+            onSemesterChange={setSelectedSemester}
+            semesterList={semesterList}
+          />
         </div>
         <div className="grid grid-cols-4 gap-4 my-4">
-          {displayClasses.map((studyClass, index) => (
+          {data.study.map((studyClass, index) => (
             <StudentCard
               key={index}
               id={studyClass.classId}
               class_name={studyClass.courseName}
               class_id={studyClass.courseId}
               image={studyClass.image}
-              semester={studyClass.semester}
+              semester={selectedSemester}
             />
           ))}
         </div>
