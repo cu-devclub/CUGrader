17a6cd69b92666de5ef654b984c679ced9b64fdb
PingZaMostIndiesBOI <73052182+OverZone-Production@users.noreply.github.com>
2025-06-10 19:02:59 +0700

    student page completed

--- Frontend/grader-frontend/src/app/instructor/page.tsx
+++ Frontend/grader-frontend/src/app/instructor/page.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import { ClassCard } from '@/components/class-card';
-import { Button } from '@/components/ui/button';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { Plus } from 'lucide-react';
-import { useState } from 'react';
-import { CreateClassDialog } from './create-class-dialog';
-import { SemesterSelector } from './semester-selector';
-import { api } from '@/lib/api';
+import { ClassCard } from "@/components/class-card";
+import { Button } from "@/components/ui/button";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { CreateClassDialog } from "./create-class-dialog";
+import { SemesterSelector } from "./semester-selector";
+import { api } from "@/lib/api";
 
 // TODO: rename this
 export default function InstructorPage() {
   const { data: semesterList } = useSuspenseQuery({
-    queryKey: ['semester'],
+    queryKey: ["semester"],
     queryFn: () => api.semester.list(),
   });
 
   const [selectedSemester, setSelectedSemester] = useState(semesterList[0]);
 
   const { data: classes } = useSuspenseQuery({
-    queryKey: ['class', selectedSemester],
+    queryKey: ["class", selectedSemester],
     queryFn: () => api.class.listBySemester(selectedSemester),
   });
 
@@ -27,22 +27,33 @@ export default function InstructorPage() {
 
   return (
     <>
-      <CreateClassDialog open={showCreateClassDialog} onOpenChange={setShowCreateClassDialog} />
-      <div className='flex flex-col p-4 gap-3'>
-        <div className='flex justify-between items-center'>
-          <div className='flex items-center gap-6'>
-            <h1 className='text-3xl font-medium text-primary'> All Classes </h1>
-            <SemesterSelector semester={selectedSemester} onSemesterChange={setSelectedSemester} semesterList={semesterList} />
+      <CreateClassDialog
+        open={showCreateClassDialog}
+        onOpenChange={setShowCreateClassDialog}
+      />
+      <div className="flex flex-col p-4 gap-3">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-6">
+            <h1 className="text-3xl font-medium text-primary"> All Classes </h1>
+            <SemesterSelector
+              semester={selectedSemester}
+              onSemesterChange={setSelectedSemester}
+              semesterList={semesterList}
+            />
           </div>
-          <Button variant="ghost" size="icon" onClick={() => setShowCreateClassDialog(true)}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setShowCreateClassDialog(true)}
+          >
             <Plus />
           </Button>
         </div>
 
         <hr />
 
-        <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(16rem,1fr))]'>
-          {classes.assistant.map(it =>
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(16rem,1fr))]">
+          {classes.assistant.map((it) => (
             <ClassCard
               key={it.classId}
               courseId={it.courseId}
@@ -52,7 +63,7 @@ export default function InstructorPage() {
               headerImageUrl={it.image}
               semester={selectedSemester}
             />
-          )}
+          ))}
         </div>
       </div>
     </>
--- Frontend/grader-frontend/src/components/class-card.tsx
+++ Frontend/grader-frontend/src/components/class-card.tsx
@@ -1,6 +1,13 @@
 import { EllipsisVertical, LucideIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,17 +19,17 @@ import {
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-
 type MenuItem = {
-  title: string,
-  icon: LucideIcon,
+  title: string;
+  icon: LucideIcon;
 } & (
-    {
+  | {
       action: () => any;
-    } | {
+    }
+  | {
       href: string;
     }
-  );
+);
 
 interface MenuProps {
   items: MenuItem[];
@@ -56,31 +63,59 @@ export interface ClassCardProps {
 }
 
 interface WithHrefProps {
-  children: React.ReactNode,
+  children: React.ReactNode;
   href?: string;
 }
 
 function WithHref({ children, href }: WithHrefProps) {
-  return href ? <Link href={href} className="hover:underline underline-offset-2 flex-1"> {children}</Link > : children;
+  return href ? (
+    <Link href={href} className="hover:underline underline-offset-2 flex-1">
+      {" "}
+      {children}
+    </Link>
+  ) : (
+    children
+  );
 }
 
-export function ClassCard({ className, courseId, href, name, semester, headerImageUrl, menuItems }: ClassCardProps) {
+export function ClassCard({
+  className,
+  courseId,
+  href,
+  name,
+  semester,
+  headerImageUrl,
+  menuItems,
+}: ClassCardProps) {
   return (
-    <Card className={cn("pt-0 pb-4 overflow-clip gap-2 hover:shadow-lg transition-shadow", className)}>
+    <Card
+      className={cn(
+        "pt-0 pb-4 overflow-clip gap-2 hover:shadow-lg transition-shadow",
+        className
+      )}
+    >
       <CardHeader className="bg-blue-500 h-28 p-0">
-        <img src={headerImageUrl} alt="" className="h-28 w-full object-cover" />
+        {headerImageUrl ? (
+          <img
+            src={headerImageUrl}
+            alt=""
+            className="h-28 w-full object-cover"
+          />
+        ) : (
+          <div className="h-28 w-full bg-blue-500" />
+        )}
       </CardHeader>
       <CardContent className="pl-4 pr-2">
         <div className="flex justify-between items-center h-9">
           <WithHref href={href}>
-            <CardTitle>{name} ({semester})</CardTitle>
+            <CardTitle>
+              {name} ({semester})
+            </CardTitle>
           </WithHref>
           {menuItems && <Menu items={menuItems} />}
         </div>
         <WithHref href={href}>
-          <CardDescription>
-            {courseId}
-          </CardDescription>
+          <CardDescription>{courseId}</CardDescription>
         </WithHref>
       </CardContent>
     </Card>
