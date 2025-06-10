10725a53cd27a1318e2673313af3763b02db6dd1
Patsagorn <patsagorn_y@outlook.com>
2025-06-10 17:42:51 +0700

    feat: Implement GetSemstersByUserId function to retrieve semesters for a user

---
+++ backend/versions/v1/models/class/getSemesters.go
@@ -0,0 +1,35 @@
+package class
+
+func (m *ClassModel) GetSemstersByUserId(userId int) ([]SemesterModel, error) {
+	var semester []SemesterModel
+	rows, err := m.DB.Query(`SELECT DISTINCT
+			cl.year "year",
+			cl.semester "semester"
+		FROM class cl
+			LEFT JOIN class_student cs ON cl.id = cs.class_id
+			LEFT JOIN class_assistant ca ON cl.id = ca.class_id
+		WHERE
+			cs.user_id = $1
+			OR ca.user_id = $1
+		ORDER BY cl.year ASC, cl.semester ASC;
+		`, userId)
+	
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var s SemesterModel
+		if err := rows.Scan(&s.Year, &s.Semester); err != nil {
+			return nil, err
+		}
+		semester = append(semester, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return semester, nil
+}
