d8bfa2eadac1de49adc0999de4db8dccbf493199
katoretb <katoretv@gmail.com>
2025-06-12 01:18:44 +0700

    remove bearer prfix

--- backend/versions/v1/models/utils/utils.go
+++ backend/versions/v1/models/utils/utils.go
@@ -14,6 +14,11 @@ type UtilsModel struct {
 
 // Verify_JWT this function will check that input token is valid with JWT secret key return error if not valid and nil if it valid
 func (um *UtilsModel) Verify_JWT(token string) error {
+	const bearerPrefix = "Bearer "
+	if len(token) > len(bearerPrefix) && token[:len(bearerPrefix)] == bearerPrefix {
+		token = token[len(bearerPrefix):]
+	}
+
 	tokenObj, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok || t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
 			return nil, errors.New("unexpected signing method")
