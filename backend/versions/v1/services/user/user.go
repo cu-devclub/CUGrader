7194ca3b81df5adac4d1160eba45c71a20ce7271
Patsagorn <patsagorn_y@outlook.com>
2025-06-13 00:00:49 +0700

    refactor: Remove unused JWTCustomClaims struct and JWT import from user service

--- backend/versions/v1/services/user/user.go
+++ backend/versions/v1/services/user/user.go
@@ -3,8 +3,6 @@ package user
 import (
 	UserModel "CUGrader/backend/versions/v1/models/user"
 	"crypto/rsa"
-
-	"github.com/golang-jwt/jwt/v4"
 )
 
 type UserService struct {
@@ -12,14 +10,4 @@ type UserService struct {
 	PrivKey *rsa.PrivateKey
 	IsDev   bool
 	JWT_Key []byte
-}
-
-type JWTCustomClaims struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Email     string `json:"email"`
-	UserID    int    `json:"user_id"`
-	Profile   string `json:"profile"`
-	Role      string `json:"role"`
-	jwt.RegisteredClaims
 }
