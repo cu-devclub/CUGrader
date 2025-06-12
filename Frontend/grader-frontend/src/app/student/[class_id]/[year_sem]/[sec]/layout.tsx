e667ee6bdb18b711bd67212aaf6d28a393ce3349
PingZaMostIndiesBOI <73052182+OverZone-Production@users.noreply.github.com>
2025-06-12 19:49:35 +0700

    Clean up code

--- Frontend/grader-frontend/src/app/student/[class_id]/[year_sem]/[sec]/layout.tsx
+++ Frontend/grader-frontend/src/app/student/[class_id]/[year_sem]/[sec]/layout.tsx
@@ -1,4 +1,3 @@
-// student/[class_id]/[year_sem]/[sec]/layout.tsx
 "use client";
 
 import Link from "next/link";
@@ -81,47 +80,3 @@ export default function StudentLayout({ children }: { children: ReactNode }) {
     </div>
   );
 }
-// <div className="flex min-h-screen">
-//   <div className="w-1/6 border-gray-300 flex flex-col items-center bg-primary">
-//     <div className="min-w-full flex px-2 py-4 items-center">
-//       <Button variant="ghost" className="mb-4 text-white">
-//         ☰
-//       </Button>
-//     </div>
-
-//     <div className="min-w-full flex px-2 pb-7 gap-y-3 items-start flex-col">
-//       <Button
-//         className="flex w-full h-1/2 text-white justify-start"
-//         variant="ghost"
-//       >
-//         Notification
-//       </Button>
-//       <Button
-//         className="flex w-full h-1/2 text-white justify-start"
-//         variant="ghost"
-//       >
-//         Profile
-//       </Button>
-//     </div>
-
-//     <div className="min-w-full flex px-6 pb-7 gap-y-3 items-start flex-col bg-white"></div>
-//   </div>
-
-//   <div className="w-5/6 border-gray-300 flex flex-col items-center">
-//     <div className="flex flex-row min-w-full h-20 space-x-5 px-24">
-//       <Button
-//         className="flex self-end h-13 w-30 bg-primary justify-start items-center px-2"
-//         // onClick={() => setActiveTab("assignment")}
-//       >
-//         Assignment
-//       </Button>
-//       <Button
-//         variant="ghost"
-//         className="self-end h-13 w-30"
-//         onClick={() => toProfilePage()}
-//       >
-//         profile
-//       </Button>
-//     </div>
-//   </div>
-// </div>
