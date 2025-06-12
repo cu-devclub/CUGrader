ea12652468b119f3ef934e70ea37b1d547ddbeae
Patsagorn <patsagorn_y@outlook.com>
2025-06-13 00:01:01 +0700

    refactor: Update GetJWTClaims to return structured JWTCustomClaims instead of jwt.MapClaims

--- backend/versions/v1/models/utils/utils.go
+++ backend/versions/v1/models/utils/utils.go
@@ -12,6 +12,16 @@ type UtilsModel struct {
 	JWT_KEY []byte
 }
 
+type JWTCustomClaims struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	UserID    int    `json:"user_id"`
+	Profile   string `json:"profile"`
+	Role      string `json:"role"`
+	jwt.RegisteredClaims
+}
+
 // Verify_JWT this function will check that input token is valid with JWT secret key return error if not valid and nil if it valid
 func (um *UtilsModel) Verify_JWT(token string) error {
 	const bearerPrefix = "Bearer "
@@ -34,12 +44,15 @@ func (um *UtilsModel) Verify_JWT(token string) error {
 	return nil
 }
 
-func (um *UtilsModel) GetJWTClaims(token string) (jwt.MapClaims, error) {
+// GetJWTClaims this function will return the claims of the JWT token if it is valid, otherwise it will return an error
+// It also strips the "Bearer " prefix if present.
+func (um *UtilsModel) GetJWTClaims(token string) (*JWTCustomClaims, error) {
 	const bearerPrefix = "Bearer "
 	if len(token) > len(bearerPrefix) && token[:len(bearerPrefix)] == bearerPrefix {
 		token = token[len(bearerPrefix):]
 	}
-	tokenObj, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+	claims := &JWTCustomClaims{}
+	tokenObj, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok || t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
 			return nil, errors.New("unexpected signing method")
 		}
@@ -50,10 +63,6 @@ func (um *UtilsModel) GetJWTClaims(token string) (jwt.MapClaims, error) {
 	}
 	if !tokenObj.Valid {
 		return nil, errors.New("invalid token")
-	}
-	claims, ok := tokenObj.Claims.(jwt.MapClaims)
-	if !ok {
-		return nil, errors.New("invalid claims")
 	}
 	return claims, nil
 }
