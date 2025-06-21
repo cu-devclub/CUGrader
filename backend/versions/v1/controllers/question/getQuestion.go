8739ad367a2660921529dd73c06687cc05794baa
Patsagorn <patsagorn_y@outlook.com>
2025-06-21 16:34:13 +0700

    refactor(GetQuestionForStudentController): streamline access control for instructors and students

--- backend/versions/v1/controllers/question/getQuestion.go
+++ backend/versions/v1/controllers/question/getQuestion.go
@@ -23,18 +23,39 @@ func (qc *QuestionController) GetQuestionForStudentController(c *gin.Context) {
 		return
 	}
 
-	if claims.Role != "student" {
-		c.JSON(http.StatusForbidden, gin.H{"error": "You are not allowed to access this resource"})
-		return
-	}
-
 	isStudentAssigned, err := qc.Service.Model.IsStudentAssignedToQuestion(questionId, claims.UserID)
-
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
 		return
 	}
 
+	isAdminOrTeacher := qc.Service.Utils.IsUserAdminOrTeacher(claims.UserID)
+	isTeachingAssistant, err := qc.Service.Model.IsUserAnAssistantToQuestion(questionId, claims.UserID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	isInstructor := isAdminOrTeacher || isTeachingAssistant
+
+	// If the user is an instructor, they can access the question without being assigned
+	if isInstructor {
+		question, err := qc.Service.GetQuestionByIDForInstructor(questionId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		}
+
+		if question == nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Question not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, question)
+		return
+	}
+
+	// If the user is a student, check if they are assigned to the question
 	if !isStudentAssigned {
 		c.JSON(http.StatusForbidden, gin.H{"error": "You are not assigned to this question"})
 		return
