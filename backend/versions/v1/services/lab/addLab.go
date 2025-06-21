1a57f6b5f0f0b1e1bd4fdae65997871a15aefea8
Patsagorn <patsagorn_y@outlook.com>
2025-06-21 14:01:07 +0700

    feat(api/v1): change examPin type from string to int in AddLabHandler and AddLab functions

--- backend/versions/v1/services/lab/addLab.go
+++ backend/versions/v1/services/lab/addLab.go
@@ -2,7 +2,7 @@ package lab
 
 import "time"
 
-func (lc *LabService) AddLab(classID int, questionNumber int, name string, publishDate time.Time, dueDate time.Time, closeOnDueDate bool, examMode bool, showScoreOnLock bool, examPin string) (int, error) {
+func (lc *LabService) AddLab(classID int, questionNumber int, name string, publishDate time.Time, dueDate time.Time, closeOnDueDate bool, examMode bool, showScoreOnLock bool, examPin int) (int, error) {
 	// TODO(ptsgrn): change testcaseID to the reasonate value
 	return lc.Model.InsertLab(classID, questionNumber, name, publishDate, dueDate, closeOnDueDate, examMode, showScoreOnLock, examPin, 0)
 }
