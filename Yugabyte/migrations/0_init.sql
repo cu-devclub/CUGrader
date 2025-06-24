acaa729d48d19431eed7213e9a4c3ffca1ccc892
Patsagorn <patsagorn_y@outlook.com>
2025-06-25 01:56:06 +0700

    refactor(yugabyte): migrate initial database setup to new migration file

---
+++ Yugabyte/migrations/0_init.sql
@@ -0,0 +1,143 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    picture VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "student" (
+    user_id INT PRIMARY KEY REFERENCES "user" (id) ON DELETE CASCADE,
+    student_id VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "teacher" (
+    user_id INT PRIMARY KEY REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "admin" (
+    user_id INT PRIMARY KEY REFERENCES "user" (id) ON DELETE CASCADE,
+    added_by INT REFERENCES "user" (id) ON DELETE SET NULL,
+    is_superadmin BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "picture" (
+    id SERIAL PRIMARY KEY,
+    path VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "class" (
+    id SERIAL PRIMARY KEY,
+    course_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    semester INT NOT NULL,
+    year INT NOT NULL,
+    picture_id INT REFERENCES "picture" (id) ON DELETE SET NULL,
+    creator_user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "class_assistant" (
+    id SERIAL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES "class" (id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+    is_leader BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "section" (
+    id SERIAL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES "class" (id) ON DELETE CASCADE,
+    section_number INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "group" (
+    id SERIAL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES "class" (id) ON DELETE CASCADE,
+    group_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "class_student" (
+    id SERIAL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES "class" (id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+    section_id INT NOT NULL REFERENCES "section" (id) ON DELETE CASCADE,
+    group_id INT REFERENCES "group" (id) ON DELETE CASCADE DEFAULT NULL,
+    withdrawn BOOLEAN NOT NULL DEFAULT FALSE,
+    withdrawn_at TIMESTAMP DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "system_language" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    service_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "lab" (
+    id SERIAL PRIMARY KEY,
+    class_id INT NOT NULL REFERENCES "class" (id) ON DELETE CASCADE,
+    number INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    publish TIMESTAMP NOT NULL,
+    due TIMESTAMP NOT NULL,
+    close_on_due BOOLEAN NOT NULL DEFAULT FALSE,
+    exam_mode BOOLEAN NOT NULL DEFAULT FALSE,
+    exam_pin VARCHAR(20) DEFAULT NULL,
+    show_score_on_lock BOOLEAN NOT NULL DEFAULT FALSE,
+    testcase_id INT NOT NULL REFERENCES "testcase" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "lab_language" (
+    lab_id INT NOT NULL REFERENCES "lab" (id) ON DELETE CASCADE,
+    system_language_id INT NOT NULL REFERENCES "system_language" (id) ON DELETE CASCADE,
+    PRIMARY KEY (lab_id, system_language_id)
+);
+
+CREATE TABLE IF NOT EXISTS "assign_to" (
+    id SERIAL PRIMARY KEY,
+    lab_id INT NOT NULL REFERENCES "lab" (id) ON DELETE CASCADE,
+    group_id INT NOT NULL REFERENCES "group" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "question" (
+    id SERIAL PRIMARY KEY,
+    lab_id INT NOT NULL REFERENCES "lab" (id) ON DELETE CASCADE,
+    number INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    score INT NOT NULL,
+    description CHAR(24) NOT NULL,
+    answer CHAR(24) NOT NULL,
+    predefine CHAR(24) NOT NULL,
+    testcase_id INT NOT NULL REFERENCES "testcase" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "testcase" (
+    id SERIAL PRIMARY KEY,
+    testcase_object_id CHAR(24) NOT NULL,
+    secret_testcase_object_id CHAR(24) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "multilang_testcase" (
+    id SERIAL PRIMARY KEY,
+    question_id INT NOT NULL REFERENCES "question" (id) ON DELETE CASCADE,
+    object_id CHAR(24) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "multilang_secret_testcase" (
+    id SERIAL PRIMARY KEY,
+    question_id INT NOT NULL REFERENCES "question" (id) ON DELETE CASCADE,
+    object_id CHAR(24) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "addition_files" (
+    id SERIAL PRIMARY KEY,
+    lab_id INT NOT NULL REFERENCES "lab" (id) ON DELETE CASCADE,
+    path VARCHAR(255) NOT NULL
+);
