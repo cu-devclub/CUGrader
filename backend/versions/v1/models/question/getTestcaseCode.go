ce6f7447d0aa33d8052ee32d13c51d959e4a4b07
Patsagorn <patsagorn_y@outlook.com>
2025-06-27 23:58:31 +0700

    feat: implement GetTestcaseContent function for retrieving testcase content with associated code

--- backend/versions/v1/models/question/getTestcaseCode.go
+++ backend/versions/v1/models/question/getTestcaseCode.go
@@ -11,6 +11,10 @@ func (m *QuestionModel) getCodeCollection() *mongo.Collection {
 	return m.MongoDB.Database("cugrader").Collection("code")
 }
 
+func (m *QuestionModel) GetTestcaseCollection() *mongo.Collection {
+	return m.MongoDB.Database("cugrader").Collection("testcase")
+}
+
 func (m *QuestionModel) GetCodeContent(ctx context.Context, objectID string) (string, error) {
 	collection := m.getCodeCollection()
 	filter := bson.M{"_id": objectID}
@@ -28,4 +32,29 @@ func (m *QuestionModel) GetCodeContent(ctx context.Context, objectID string) (st
 	}
 
 	return result.Content, nil
+}
+
+func (m *QuestionModel) GetTestcaseContent(ctx context.Context, testcaseObjectID string) (MultilangTestcase, error) {
+	collection := m.GetTestcaseCollection()
+	filter := bson.M{"_id": testcaseObjectID}
+
+	var testcaseMongoObject TestcaseMongoModel
+	err := collection.FindOne(ctx, filter).Decode(&testcaseMongoObject)
+	if err != nil {
+		return MultilangTestcase{}, err // Other error
+	}
+
+	inputCodeContent, err := m.GetCodeContent(ctx, testcaseMongoObject.Input)
+	if err != nil {
+		return MultilangTestcase{}, err
+	}
+	outputCodeContent, err := m.GetCodeContent(ctx, testcaseMongoObject.Output)
+	if err != nil {
+		return MultilangTestcase{}, err
+	}
+
+	return MultilangTestcase{
+		Input:  inputCodeContent,
+		Output: outputCodeContent,
+	}, nil
 }
